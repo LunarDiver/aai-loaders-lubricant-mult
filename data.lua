06dd84f8d927ef7622d91d60186fea23850a12d8
Luna <dev@lunardiver.com>
2024-05-16 20:25:45 +0200

    Added setting to change multiplier

--- data.lua
+++ data.lua
@@ -1,6 +1,6 @@
 local old_function = AAILoaders.make_tier
 AAILoaders.make_tier = function(input)
-    local new_consumption = input.fluid_per_minute * 10
+    local new_consumption = input.fluid_per_minute * settings.startup["aaillm-mult"].value
     log("Changing " .. input.name .. " fluid consumption from " .. input.fluid_per_minute .. " to " .. new_consumption)
     input.fluid_per_minute = new_consumption
     old_function(input)
---
+++ settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "double-setting",
+        name = "aaillm-mult",
+        setting_type = "startup",
+        default_value = 10,
+        minimum_value = 0,
+        maximum_value = 1000000
+    }
+})
